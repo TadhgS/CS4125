1059e20cdd6ac1998a8712aac5b103198a7ce046
danie <danie@10.52.242.42>
2017-11-13 12:45:30 +0000

    Added customer classifications no implementation yet.

--- CS4125/src/customer/Customer.java
+++ CS4125/src/customer/Customer.java
@@ -1,5 +1,41 @@
 package customer;
 
-public class Customer {
+import java.util.ArrayList;
+import userinterface.File;
 
+public class Customer 
+{
+	public static int customer(String customerID)
+	{
+		File Desc = new File(2);
+		ArrayList<String> loadedCustomers = Desc.getFile();
+		
+		int check = loadedCustomers.indexOf(customerID);
+		
+		if(check == -1)
+		{
+			return -1;
+		}
+		else
+		{
+			String spent = "";
+			spent.equals(loadedCustomers.get(check += 1));
+			double value = Double.parseDouble(spent);
+		
+			if(value >= 100.00 || value <= 199.99)
+			{
+				return 1;
+			}
+			else if(value >= 200.00 || value <= 299.99)
+			{
+				return 2;
+			}
+			else if(value > 300.00)
+			{
+				return 3;
+			}
+			else
+				return 0;
+		}
+	}
 }
--- CS4125/src/payment/Transaction.java
+++ CS4125/src/payment/Transaction.java
@@ -1,5 +1,60 @@
 package payment;
 
-public class Transaction {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import customer.Customer;
 
+public class Transaction extends JFrame implements ActionListener
+{
+	//private static final long serialVersionUID = 1L;
+	private JButton customerIDButton;
+	private JTextField userText;
+	private String customerID = "";
+	private static int checkID;
+	
+	public Transaction()
+	{
+		setTitle("Customer ID");
+        setLayout(null);
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        JLabel userLabel = new JLabel("Customer ID:");
+        userLabel.setBounds(10, 10, 80, 25);
+        add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100, 10, 160, 25);
+        add(userText);
+
+        customerIDButton = new JButton("Login");
+        customerIDButton.setBounds(180, 80, 80, 25);
+        customerIDButton.addActionListener(this);
+        add(customerIDButton);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		if (e.getSource() == customerIDButton)
+        {
+            customerID = userText.getText();
+            checkID = Customer.customer(customerID);
+            
+            switch(checkID)
+            {
+            	case -1: JOptionPane.showMessageDialog(null, "No ID exists");//call to make random customer ID
+            		break;
+            	case 0: JOptionPane.showMessageDialog(null, "Not enough has been spent for a discount");
+        			break;
+            	case 1: //set discount to 10%
+        			break;
+            	case 2: //set discount to 15%
+        			break;
+            	case 3: //set discount to 20%
+            		break;
+            }
+        }
+	}
 }
